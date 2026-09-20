aa18c4236abd0d0bb5d49b077afb8226e84cd4e2
Umer Khan <umar_s66@outlook.com>
2026-09-20 14:04:04 +0300

    Add some secret

---
+++ DelegationWithCombine/Secret.swift
@@ -0,0 +1,10 @@
+//
+//  Secret.swift
+//  DelegationWithCombine
+//
+//  Created by Umer Khan on 20.09.2026.
+//  Copyright © 2026 Umer Khan. All rights reserved.
+//
+
+import Foundation
+let someSecret = "axyzabct-abdxadta-ddxddccc"
